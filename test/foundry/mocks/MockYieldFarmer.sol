2e401498cddc8e597f7b1ef541c9dc7edc81f649
JChoy <jchoyeclipse@gmail.com>
2023-07-04 16:02:31 +0900

    test: add withdrawalLimit at MockYieldFarmer

--- test/foundry/mocks/MockYieldFarmer.sol
+++ test/foundry/mocks/MockYieldFarmer.sol
@@ -8,9 +8,13 @@ import "../../../contracts/interfaces/IYieldFarmer.sol";
 
 contract MockYieldFarmer is IYieldFarmer {
     mapping(address => uint256) public override assetBalance;
+    mapping(address => uint256) public withdrawLimit;
 
-    function withdrawable(address asset) external view returns (uint256) {
-        return assetBalance[asset];
+    function withdrawable(address asset) external view returns (uint256 amount) {
+        amount = assetBalance[asset];
+        if (withdrawLimit[asset] > 0 && amount > withdrawLimit[asset]) {
+            amount = withdrawLimit[asset];
+        }
     }
 
     function deposit(address asset, uint256 amount) external {
@@ -22,5 +26,9 @@ contract MockYieldFarmer is IYieldFarmer {
         require(assetBalance[asset] >= amount, "insufficient balance");
         IERC20(asset).transfer(msg.sender, amount);
         assetBalance[asset] -= amount;
+    }
+
+    function setWithdrawLimit(address asset, uint256 amount) external {
+        withdrawLimit[asset] = amount;
     }
 }
