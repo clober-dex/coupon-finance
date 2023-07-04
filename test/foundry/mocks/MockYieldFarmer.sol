bd32b8cd693a19dd6ae1b558b1196752b8676fa9
JChoy <jchoyeclipse@gmail.com>
2023-07-04 15:07:49 +0900

    test: add MockYieldFarmer

---
+++ test/foundry/mocks/MockYieldFarmer.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "../../../contracts/interfaces/IYieldFarmer.sol";
+
+contract MockYieldFarmer is IYieldFarmer {
+    mapping(address => uint256) public override assetBalance;
+
+    function withdrawableBalance(address asset) external view returns (uint256) {
+        return assetBalance[asset];
+    }
+
+    function deposit(address asset, uint256 amount) external {
+        IERC20(asset).transferFrom(msg.sender, address(this), amount);
+        assetBalance[asset] += amount;
+    }
+
+    function withdraw(address asset, uint256 amount) external {
+        require(assetBalance[asset] >= amount, "insufficient balance");
+        IERC20(asset).transfer(msg.sender, amount);
+        assetBalance[asset] -= amount;
+    }
+}
