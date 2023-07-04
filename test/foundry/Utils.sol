64e651b21ae9eb149fb035bf273e3b37a4ad27df
JChoy <jchoyeclipse@gmail.com>
2023-07-04 16:02:12 +0900

    test: add ERC20Utils for test

---
+++ test/foundry/Utils.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+library ERC20Utils {
+    function amount(IERC20 token, uint256 ethers) internal view returns (uint256) {
+        return ethers * 10 ** (IERC20Metadata(address(token)).decimals());
+    }
+}
