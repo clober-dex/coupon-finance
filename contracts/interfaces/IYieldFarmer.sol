ce612dd641463b3d1d911070291bac71cadfa973
JChoy <jchoyeclipse@gmail.com>
2023-07-05 17:27:25 +0900

    feat: change IYieldFarmer not to care the user

--- contracts/interfaces/IYieldFarmer.sol
+++ contracts/interfaces/IYieldFarmer.sol
@@ -5,15 +5,11 @@ pragma solidity ^0.8.0;
 interface IYieldFarmer {
     function totalReservedAmount(address asset) external view returns (uint256);
 
-    function reservedAmount(address asset, address user) external view returns (uint256);
+    function reservedAmount(address asset) external view returns (uint256);
 
     function withdrawable(address asset) external view returns (uint256);
 
-    function claimable(address asset, address user) external view returns (uint256);
-
-    function deposit(address asset, uint256 amount, address recipient) external;
+    function deposit(address asset, uint256 amount) external;
 
     function withdraw(address asset, uint256 amount, address recipient) external;
-
-    function claim(address asset, address user) external;
 }
--- test/foundry/mocks/MockYieldFarmer.sol
+++ test/foundry/mocks/MockYieldFarmer.sol
@@ -8,8 +8,7 @@ import "../../../contracts/interfaces/IYieldFarmer.sol";
 
 contract MockYieldFarmer is IYieldFarmer {
     mapping(address asset => uint256) public override totalReservedAmount;
-    mapping(address asset => mapping(address user => uint256 amount)) public override reservedAmount;
-    mapping(address asset => mapping(address user => uint256 amount)) public override claimable;
+    mapping(address asset => uint256) public override reservedAmount;
     mapping(address => uint256) public withdrawLimit;
 
     function withdrawable(address asset) external view returns (uint256 amount) {
@@ -19,23 +18,17 @@ contract MockYieldFarmer is IYieldFarmer {
         }
     }
 
-    function deposit(address asset, uint256 amount, address recipient) external {
+    function deposit(address asset, uint256 amount) external {
         IERC20(asset).transferFrom(msg.sender, address(this), amount);
         totalReservedAmount[asset] += amount;
-        reservedAmount[asset][recipient] += amount;
+        reservedAmount[asset] += amount;
     }
 
     function withdraw(address asset, uint256 amount, address recipient) external {
         require(totalReservedAmount[asset] >= amount, "insufficient balance");
         IERC20(asset).transfer(recipient, amount);
         totalReservedAmount[asset] -= amount;
-        reservedAmount[asset][recipient] -= amount;
-    }
-
-    function claim(address asset, address user) external {
-        uint256 amount = claimable[asset][user];
-        IERC20(asset).transfer(user, amount);
-        claimable[asset][user] = 0;
+        reservedAmount[asset] -= amount;
     }
 
     function setWithdrawLimit(address asset, uint256 amount) external {
