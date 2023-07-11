6bb8ae4b44f585b770d1458ad4ed4356a3a67d71
JChoy <jchoyeclipse@gmail.com>
2023-07-11 11:27:37 +0900

    test: handle native withdraw at MockYieldFarmer

--- test/foundry/unit/LendingPool/SetUp.sol
+++ test/foundry/unit/LendingPool/SetUp.sol
@@ -36,7 +36,7 @@ library SetUp {
         res.weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
         res.usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
-        res.yieldFarmer = new MockYieldFarmer();
+        res.yieldFarmer = new MockYieldFarmer(address(res.weth));
         res.oracle = new MockOracle();
         res.lendingPool = new LendingPool(
             Constants.MAX_EPOCH_DIFF,
