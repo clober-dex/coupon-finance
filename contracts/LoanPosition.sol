4017101c0cd34172141dfd7d0290efac6320ff55
Sean <detectivekim1@gmail.com>
2023-07-14 12:57:20 +0900

    fix: add `Math.sol`

--- contracts/LoanPosition.sol
+++ contracts/LoanPosition.sol
@@ -7,6 +7,7 @@ import {ERC721Permit} from "./libraries/ERC721Permit.sol";
 import {ReentrancyGuard} from "./libraries/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {Types} from "./Types.sol";
 import {ILoanPosition} from "./interfaces/ILoanPosition.sol";
 import {IAssetPool} from "./interfaces/IAssetPool.sol";
