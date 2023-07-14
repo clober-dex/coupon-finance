9afc8d340d47d3ee842a1221fe5639c9dbb3caa4
JChoy <jchoyeclipse@gmail.com>
2023-07-14 14:42:20 +0900

    feat: add ACCESS and EXPIRED_EPOCH Error

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -4,6 +4,8 @@
 pragma solidity ^0.8.0;
 
 library Errors {
+    bytes internal constant ACCESS = "ACCESS";
+    bytes internal constant EXPIRED_EPOCH = "EXPIRED_EPOCH";
     bytes internal constant UNREGISTERED_ASSET = "UNREGISTERED_ASSET";
     bytes internal constant TOO_SMALL_DEBT = "TOO_SMALL_DEBT";
     bytes internal constant LIQUIDATION_THRESHOLD = "LIQUIDATION_THRESHOLD";
