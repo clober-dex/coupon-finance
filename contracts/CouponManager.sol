1ca4821e902e145021a1a1ff7946678cc71ccebc
JChoy <jchoyeclipse@gmail.com>
2023-07-18 11:38:31 +0900

    feat: implement CouponManager

--- contracts/CouponManager.sol
+++ contracts/CouponManager.sol
@@ -7,12 +7,17 @@ import {IERC1155MetadataURI} from "@openzeppelin/contracts/token/ERC1155/extensi
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
+import {Errors} from "./Errors.sol";
 import {Types} from "./Types.sol";
+import {CouponKey} from "./libraries/CouponKey.sol";
+import {Coupon} from "./libraries/Coupon.sol";
 import {Epoch} from "./libraries/Epoch.sol";
 import {ERC1155Permit} from "./libraries/ERC1155Permit.sol";
 import {ICouponManager} from "./interfaces/ICouponManager.sol";
 
 contract CouponManager is ERC1155Permit, ERC1155Supply, ICouponManager {
+    using CouponKey for Types.CouponKey;
+    using Coupon for Types.Coupon;
     using Epoch for Types.Epoch;
 
     address public immutable override minter;
@@ -21,11 +26,19 @@ contract CouponManager is ERC1155Permit, ERC1155Supply, ICouponManager {
 
     constructor(address minter_, string memory uri_) ERC1155Permit(uri_, "Coupon", "1") {
         minter = minter_;
+        baseURI = uri_;
+    }
+
+    modifier onlyMinter() {
+        if (msg.sender != minter) {
+            revert(Errors.ACCESS);
+        }
+        _;
     }
 
     // View Functions //
     function uri(uint256 id) public view override(ERC1155, IERC1155MetadataURI) returns (string memory) {
-        revert("not implemented");
+        revert("not implemented"); // TODO
     }
 
     function currentEpoch() external view returns (Types.Epoch) {
@@ -51,20 +64,44 @@ contract CouponManager is ERC1155Permit, ERC1155Supply, ICouponManager {
         Types.Coupon[] calldata coupons,
         bytes calldata data
     ) external {
-        revert("not implemented");
+        (uint256[] memory ids, uint256[] memory amounts) = _splitCoupons(coupons);
+        safeBatchTransferFrom(from, to, ids, amounts, data);
     }
 
     function burnExpiredCoupons(Types.CouponKey[] calldata couponKeys) external {
-        revert("not implemented");
+        uint256[] memory ids = new uint256[](couponKeys.length);
+        uint256[] memory amounts = new uint256[](couponKeys.length);
+        Types.Epoch currentEpoch = Epoch.current();
+        uint256 count;
+        for (uint256 i = 0; i < couponKeys.length; ++i) {
+            if (couponKeys[i].epoch.compare(currentEpoch) >= 0) {
+                continue;
+            }
+            uint256 id = couponKeys[i].toId();
+            uint256 amount = balanceOf(msg.sender, id);
+            if (amount == 0) {
+                continue;
+            }
+            count++;
+            ids[i] = id;
+            amounts[i] = amount;
+        }
+        assembly {
+            mstore(ids, count)
+            mstore(amounts, count)
+        }
+        _burnBatch(msg.sender, ids, amounts);
     }
 
     // Admin Functions //
-    function mintBatch(address to, Types.Coupon[] calldata coupons, bytes memory data) external {
-        revert("not implemented");
+    function mintBatch(address to, Types.Coupon[] calldata coupons, bytes memory data) external onlyMinter {
+        (uint256[] memory ids, uint256[] memory amounts) = _splitCoupons(coupons);
+        _mintBatch(to, ids, amounts, data);
     }
 
-    function burnBatch(address user, Types.Coupon[] calldata coupons) external {
-        revert("not implemented");
+    function burnBatch(address user, Types.Coupon[] calldata coupons) external onlyMinter {
+        (uint256[] memory ids, uint256[] memory amounts) = _splitCoupons(coupons);
+        _burnBatch(user, ids, amounts);
     }
 
     function _beforeTokenTransfer(
@@ -76,5 +113,18 @@ contract CouponManager is ERC1155Permit, ERC1155Supply, ICouponManager {
         bytes memory data
     ) internal override(ERC1155, ERC1155Supply) {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function _splitCoupons(
+        Types.Coupon[] calldata coupons
+    ) internal pure returns (uint256[] memory ids, uint256[] memory amounts) {
+        ids = new uint256[](coupons.length);
+        amounts = new uint256[](coupons.length);
+        unchecked {
+            for (uint256 i = 0; i < coupons.length; ++i) {
+                ids[i] = coupons[i].key.toId();
+                amounts[i] = coupons[i].amount;
+            }
+        }
     }
 }
