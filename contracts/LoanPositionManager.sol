e33fcceb7b9b229998268a5a046c68c8022c466d
Sean <detectivekim1@gmail.com>
2023-07-20 16:17:56 +0900

    feat: add event `PositionUpdated` at `liquidate` function

--- contracts/LoanPositionManager.sol
+++ contracts/LoanPositionManager.sol
@@ -398,15 +398,35 @@ contract LoanPositionManager is ILoanPositionManager, ERC721Permit, Ownable, ERC
             maxRepayAmount > 0 ? maxRepayAmount : type(uint256).max
         );
 
-        require(liquidationAmount > 0, "LIQUIDATION_FAIL");
+        require(liquidationAmount > 0 || repayAmount > 0, "LIQUIDATION_FAIL");
 
         unchecked {
-            _positionMap[tokenId].collateralAmount = position.collateralAmount - liquidationAmount - protocolFeeAmount;
-            _positionMap[tokenId].debtAmount = position.debtAmount - repayAmount;
+            Types.Epoch currentEpoch = Epoch.current();
+            address couponOwner = ownerOf(tokenId);
+            if (position.expiredWith.compare(currentEpoch) >= 0) {
+                uint256 length = position.expiredWith.sub(currentEpoch) + 1;
+                Types.Coupon[] memory coupons = new Types.Coupon[](length);
+                for (uint16 i = 0; i < length; ++i) {
+                    coupons[i] = Coupon.from(position.debtToken, currentEpoch.add(i), repayAmount);
+                }
+                try
+                    ICouponManager(couponManager).safeBatchTransferFrom(address(this), couponOwner, coupons, data)
+                {} catch {
+                    for (uint256 i = 0; i < length; ++i) {
+                        couponOwed[couponOwner][coupons[i].id()] += coupons[i].amount;
+                    }
+                }
+            }
+            position.collateralAmount -= liquidationAmount + protocolFeeAmount;
+            position.debtAmount -= repayAmount;
+            if (position.debtAmount == 0) position.expiredWith = currentEpoch.sub(1);
         }
+        _positionMap[tokenId].collateralAmount = position.collateralAmount;
+        _positionMap[tokenId].debtAmount = position.debtAmount;
 
         IAssetPool(assetPool).withdraw(position.collateralToken, liquidationAmount, msg.sender);
         IAssetPool(assetPool).withdraw(position.collateralToken, protocolFeeAmount, treasury);
+
         if (data.length > 0) {
             ILiquidateCallbackReceiver(msg.sender).couponFinanceLiquidateCallback(
                 tokenId,
@@ -419,27 +439,9 @@ contract LoanPositionManager is ILoanPositionManager, ERC721Permit, Ownable, ERC
         }
         IERC20(position.debtToken).safeTransferFrom(msg.sender, assetPool, repayAmount);
         IAssetPool(assetPool).deposit(position.debtToken, repayAmount);
-        emit PositionLiquidated(tokenId);
 
-        unchecked {
-            address couponOwner = ownerOf(tokenId);
-            Types.Epoch epoch = Epoch.current();
-            if (position.expiredWith.compare(epoch) >= 0) {
-                uint256 length = position.expiredWith.sub(epoch) + 1;
-                Types.Coupon[] memory coupons = new Types.Coupon[](length);
-                for (uint256 i = 0; i < length; ++i) {
-                    coupons[i] = Coupon.from(position.debtToken, epoch, repayAmount);
-                    epoch = epoch.add(1);
-                }
-                try
-                    ICouponManager(couponManager).safeBatchTransferFrom(address(this), couponOwner, coupons, data)
-                {} catch {
-                    for (uint256 i = 0; i < length; ++i) {
-                        couponOwed[couponOwner][coupons[i].id()] += coupons[i].amount;
-                    }
-                }
-            }
-        }
+        emit PositionLiquidated(tokenId);
+        emit PositionUpdated(tokenId, position.collateralAmount, position.debtAmount, position.expiredWith);
     }
 
     function claimOwedCoupons(Types.CouponKey[] memory couponKeys, bytes calldata data) external {
