85b30a39b1668250d66ac05321b6ba8a380b31fc
JChoy <jchoyeclipse@gmail.com>
2023-07-21 16:56:10 +0900

    refactor: make ERC721Permit use EIP712 #1

--- contracts/libraries/ERC721Permit.sol
+++ contracts/libraries/ERC721Permit.sol
@@ -6,51 +6,23 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 import {ChainId} from "./ChainId.sol";
 import {IERC721Permit} from "../interfaces/IERC721Permit.sol";
 
-/// @title ERC721 with permit
-/// @notice Nonfungible tokens that support an approve via signature, i.e. permit
-/// @author Clober (https://github.com/clober-dex/coupon-finance/blob/main/contracts/libraries/ERC721Permit.sol)
-/// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/ERC721Permit.sol)
-abstract contract ERC721Permit is ERC721, IERC721Permit {
-    /// @dev Gets the current nonce for a token ID and then increments it, returning the original value
-    function _getAndIncrementNonce(uint256 tokenId) internal virtual returns (uint256);
-
-    /// @dev The hash of the name used in the permit signature verification
-    bytes32 private immutable nameHash;
-
-    /// @dev The hash of the version string used in the permit signature verification
-    bytes32 private immutable versionHash;
-
-    /// @notice Computes the nameHash and versionHash
-    constructor(string memory name_, string memory symbol_, string memory version_) ERC721(name_, symbol_) {
-        nameHash = keccak256(bytes(name_));
-        versionHash = keccak256(bytes(version_));
-    }
-
-    /// @inheritdoc IERC721Permit
-    function DOMAIN_SEPARATOR() public view override returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
-                    0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
-                    nameHash,
-                    versionHash,
-                    ChainId.get(),
-                    address(this)
-                )
-            );
-    }
-
-    /// @inheritdoc IERC721Permit
-    /// @dev Value is equal to keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
+abstract contract ERC721Permit is ERC721, IERC721Permit, EIP712 {
+    // keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
     bytes32 public constant override PERMIT_TYPEHASH =
         0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
 
-    /// @inheritdoc IERC721Permit
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory version_
+    ) ERC721(name_, symbol_) EIP712(name_, version_) {}
+
     function permit(
         address spender,
         uint256 tokenId,
@@ -61,28 +33,31 @@ abstract contract ERC721Permit is ERC721, IERC721Permit {
     ) external payable override {
         require(block.timestamp <= deadline, "Permit expired");
 
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, _getAndIncrementNonce(tokenId), deadline))
-            )
+        bytes32 structHash = keccak256(
+            abi.encode(PERMIT_TYPEHASH, spender, tokenId, _getAndIncrementNonce(tokenId), deadline)
         );
+        bytes32 digest = _hashTypedDataV4(structHash);
+
         address owner = ownerOf(tokenId);
         require(spender != owner, "ERC721Permit: approval to current owner");
 
         if (Address.isContract(owner)) {
             require(IERC1271(owner).isValidSignature(digest, abi.encodePacked(r, s, v)) == 0x1626ba7e, "Unauthorized");
         } else {
-            address recoveredAddress = ecrecover(digest, v, r, s);
-            require(recoveredAddress != address(0), "Invalid signature");
-            require(recoveredAddress == owner, "Unauthorized");
+            address signer = ECDSA.recover(digest, v, r, s);
+            require(signer == owner, "Unauthorized");
         }
 
         _approve(spender, tokenId);
     }
 
+    function DOMAIN_SEPARATOR() public view override returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, IERC165) returns (bool) {
         return interfaceId == type(IERC721Permit).interfaceId || super.supportsInterface(interfaceId);
     }
+
+    function _getAndIncrementNonce(uint256 tokenId) internal virtual returns (uint256);
 }
