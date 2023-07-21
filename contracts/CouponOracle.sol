8d17b75e45547b33447c5c0ddec86b004e6e9b28
Sean <detectivekim1@gmail.com>
2023-07-21 20:11:44 +0900

    feat: add `fallbackOracle` at `CouponOracle`

--- contracts/CouponOracle.sol
+++ contracts/CouponOracle.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IAaveOracle} from "./external/aave-v3/IAaveOracle.sol";
+import {IAaveOracle, IPriceOracleGetter} from "./external/aave-v3/IAaveOracle.sol";
 import "./interfaces/ICouponOracle.sol";
 
 contract CouponOracle is ICouponOracle {
-    address private immutable _oracle;
     address private immutable _weth;
+    address private immutable _oracle;
+    address private immutable _fallbackOracle;
 
-    constructor(address oracle_, address weth_) {
-        _oracle = oracle_;
+    constructor(address weth_, address oracle_, address fallbackOracle_) {
         _weth = weth_;
+        _oracle = oracle_;
+        _fallbackOracle = fallbackOracle_;
     }
 
     /**
@@ -19,21 +21,26 @@ contract CouponOracle is ICouponOracle {
      * @return The price of the asset
      */
     function getAssetPrice(address asset) external view returns (uint256) {
-        return IAaveOracle(_oracle).getAssetPrice(asset == address(0) ? _weth : asset);
+        if (asset == address(0)) asset = _weth;
+        uint256 price = IAaveOracle(_oracle).getAssetPrice(asset);
+        return price == 0 ? IPriceOracleGetter(_fallbackOracle).getAssetPrice(asset) : price;
     }
 
     /**
      * @notice Returns a list of prices from a list of assets addresses
      * @param assets The list of assets addresses
-     * @return The prices of the given assets
+     * @return prices The prices of the given assets
      */
-    function getAssetsPrices(address[] memory assets) external view returns (uint256[] memory) {
+    function getAssetsPrices(address[] memory assets) external view returns (uint256[] memory prices) {
         uint256 length = assets.length;
         unchecked {
             for (uint256 i = 0; i < length; ++i) {
                 if (assets[i] == address(0)) assets[i] = _weth;
             }
+            prices = IAaveOracle(_oracle).getAssetsPrices(assets);
+            for (uint256 i = 0; i < length; ++i) {
+                if (prices[i] == 0) prices[i] = IPriceOracleGetter(_fallbackOracle).getAssetPrice(assets[i]);
+            }
         }
-        return IAaveOracle(_oracle).getAssetsPrices(assets);
     }
 }
