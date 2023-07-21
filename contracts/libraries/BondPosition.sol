dd886befba7734eee53192c0d8b7a3f8b08b1a63
Sean <detectivekim1@gmail.com>
2023-07-21 19:47:27 +0900

    refactor: clarify `adjustPosition` function in `BondPositionLibrary` contract

--- contracts/libraries/BondPosition.sol
+++ contracts/libraries/BondPosition.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
+import {Errors} from "../Errors.sol";
 import {Epoch, EpochLibrary} from "./Epoch.sol";
 import {Coupon, CouponLibrary} from "./Coupon.sol";
 
@@ -30,96 +31,53 @@ library BondPositionLibrary {
         nonce = positionStorage.nonce++;
     }
 
-    function adjustPosition(
-        BondPosition memory position,
-        uint256 amount,
-        Epoch expiredWith,
-        Epoch latestExpiredEpoch
-    ) internal pure returns (BondPosition memory, Coupon[] memory, Coupon[] memory) {
-        position = clone(position);
-        if (amount == 0) {
-            expiredWith = latestExpiredEpoch;
-        } else {
-            if (latestExpiredEpoch.compare(expiredWith) >= 0) {
-                expiredWith = latestExpiredEpoch;
+    function calculateCouponRequirement(
+        BondPosition memory oldPosition,
+        BondPosition memory newPosition
+    ) internal view returns (Coupon[] memory, Coupon[] memory) {
+        require(oldPosition.asset == newPosition.asset && oldPosition.nonce == newPosition.nonce, Errors.INVALID_INPUT);
+
+        Epoch latestExpiredEpoch = EpochLibrary.current().sub(1);
+        uint256 mintCouponsLength = newPosition.expiredWith.sub(latestExpiredEpoch);
+        uint256 burnCouponsLength = oldPosition.expiredWith.sub(latestExpiredEpoch);
+        unchecked {
+            uint256 minCount = Math.min(mintCouponsLength, burnCouponsLength);
+            if (newPosition.amount > oldPosition.amount) {
+                burnCouponsLength -= minCount;
+            } else if (newPosition.amount < oldPosition.amount) {
+                mintCouponsLength -= minCount;
+            } else {
+                mintCouponsLength -= minCount;
+                burnCouponsLength -= minCount;
             }
         }
 
-        (uint256 mintCouponsLength, uint256 burnCouponsLength) = _calculateCouponCount(
-            position,
-            amount,
-            expiredWith,
-            latestExpiredEpoch
-        );
         Coupon[] memory mintCoupons = new Coupon[](mintCouponsLength);
         Coupon[] memory burnCoupons = new Coupon[](burnCouponsLength);
         mintCouponsLength = 0;
         burnCouponsLength = 0;
-        uint16 farthestExpiredEpochs = expiredWith.max(position.expiredWith).sub(latestExpiredEpoch);
-        for (uint16 i = 0; i < farthestExpiredEpochs; ++i) {
-            latestExpiredEpoch = latestExpiredEpoch.add(1); // reuse minEpoch as epoch
-            (uint256 mintAmount, uint256 burnAmount) = _calculateCouponAmount(
-                position,
-                amount,
-                expiredWith,
-                latestExpiredEpoch
-            );
-            if (mintAmount > 0) {
-                mintCoupons[mintCouponsLength++] = CouponLibrary.from(position.asset, latestExpiredEpoch, mintAmount);
-            }
-            if (burnAmount > 0) {
-                burnCoupons[burnCouponsLength++] = CouponLibrary.from(position.asset, latestExpiredEpoch, burnAmount);
-            }
-        }
-        position.amount = amount;
-        position.expiredWith = expiredWith;
-        return (position, mintCoupons, burnCoupons);
-    }
-
-    function _calculateCouponCount(
-        BondPosition memory position,
-        uint256 amount,
-        Epoch expiredWith,
-        Epoch latestExpiredEpoch
-    ) private pure returns (uint256 mintCount, uint256 burnCount) {
-        mintCount = expiredWith.sub(latestExpiredEpoch);
-        burnCount = position.expiredWith.sub(latestExpiredEpoch);
+        uint16 farthestExpiredEpochs = newPosition.expiredWith.max(oldPosition.expiredWith).sub(latestExpiredEpoch);
         unchecked {
-            uint256 minCount = Math.min(mintCount, burnCount);
-            if (amount > position.amount) {
-                burnCount -= minCount;
-            } else if (amount < position.amount) {
-                mintCount -= minCount;
-            } else {
-                mintCount -= minCount;
-                burnCount -= minCount;
+            for (uint16 i = 0; i < farthestExpiredEpochs; ++i) {
+                latestExpiredEpoch = latestExpiredEpoch.add(1); // reuse minEpoch as latestExpiredEpoch
+                uint256 newAmount = newPosition.expiredWith.compare(latestExpiredEpoch) < 0 ? 0 : newPosition.amount;
+                uint256 oldAmount = oldPosition.expiredWith.compare(latestExpiredEpoch) < 0 ? 0 : oldPosition.amount;
+                if (newAmount > oldAmount) {
+                    mintCoupons[mintCouponsLength++] = CouponLibrary.from(
+                        oldPosition.asset,
+                        latestExpiredEpoch,
+                        newAmount - oldAmount
+                    );
+                } else if (newAmount < oldAmount) {
+                    burnCoupons[burnCouponsLength++] = CouponLibrary.from(
+                        oldPosition.asset,
+                        latestExpiredEpoch,
+                        oldAmount - newAmount
+                    );
+                }
             }
         }
-    }
-
-    function _calculateCouponAmount(
-        BondPosition memory position,
-        uint256 amount,
-        Epoch expiredWith,
-        Epoch epoch
-    ) private pure returns (uint256 mintAmount, uint256 burnAmount) {
-        uint256 newAmount = expiredWith.compare(epoch) < 0 ? 0 : amount;
-        uint256 oldAmount = position.expiredWith.compare(epoch) < 0 ? 0 : position.amount;
-        if (newAmount > oldAmount) {
-            mintAmount = newAmount - oldAmount;
-        } else if (newAmount < oldAmount) {
-            burnAmount = oldAmount - newAmount;
-        }
-    }
-
-    function clone(BondPosition memory position) internal pure returns (BondPosition memory) {
-        return
-            BondPosition({
-                asset: position.asset,
-                nonce: position.nonce,
-                expiredWith: position.expiredWith,
-                amount: position.amount
-            });
+        return (mintCoupons, burnCoupons);
     }
 
     function compareEpoch(BondPosition memory position1, BondPosition memory position2) internal pure returns (int256) {
