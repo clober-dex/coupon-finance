13b5f6976cde0b2a8cfe7f43cad36ec17c6444bb
JChoy <jchoyeclipse@gmail.com>
2023-07-31 16:20:02 +0900

    feat: add approve to Currency

--- contracts/libraries/Currency.sol
+++ contracts/libraries/Currency.sol
@@ -19,6 +19,12 @@ library CurrencyLibrary {
     /// @notice Thrown when an ERC20 transfer fails
     error ERC20TransferFailed();
 
+    /// @notice Thrown when a call is not allowed to native
+    error NativeNotAllowed();
+
+    /// @notice Thrown when an ERC20 approve fails
+    error ERC20ApproveFailed();
+
     Currency public constant NATIVE = Currency.wrap(address(0));
 
     function transfer(Currency currency, address to, uint256 amount) internal {
@@ -67,7 +73,7 @@ library CurrencyLibrary {
         // https://github.com/transmissions11/solmate/blob/e8f96f25d48fe702117ce76c79228ca4f20206cb/src/utils/SafeTransferLib.sol
 
         if (currency.isNative()) {
-            revert NativeTransferFailed();
+            revert NativeNotAllowed();
         } else {
             bool success;
             assembly {
@@ -96,6 +102,39 @@ library CurrencyLibrary {
             }
 
             if (!success) revert ERC20TransferFailed();
+        }
+    }
+
+    function approve(Currency currency, address to, uint256 amount) internal {
+        // implementation from
+        // https://github.com/transmissions11/solmate/blob/e8f96f25d48fe702117ce76c79228ca4f20206cb/src/utils/SafeTransferLib.sol
+        if (currency.isNative()) {
+            revert NativeNotAllowed();
+        } else {
+            bool success;
+            assembly {
+                // We'll write our calldata to this slot below, but restore it later.
+                let memPointer := mload(0x40)
+
+                // Write the abi-encoded calldata into memory, beginning with the function selector.
+                mstore(0, 0x095ea7b300000000000000000000000000000000000000000000000000000000)
+                mstore(4, to) // Append the "to" argument.
+                mstore(36, amount) // Append the "amount" argument.
+
+                success := and(
+                    // Set success to whether the call reverted, if not we check it either
+                    // returned exactly 1 (can't just be non-zero data), or had no return data.
+                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                    // We use 68 because that's the total length of our calldata (4 + 32 * 2)
+                    // Counterintuitively, this call() must be positioned after the or() in the
+                    // surrounding and() because and() evaluates its arguments from right to left.
+                    call(gas(), currency, 0, 0, 68, 0, 32)
+                )
+
+                mstore(0x60, 0) // Restore the zero slot to zero.
+                mstore(0x40, memPointer) // Restore the memPointer.
+            }
+            if (!success) revert ERC20ApproveFailed();
         }
     }
 
