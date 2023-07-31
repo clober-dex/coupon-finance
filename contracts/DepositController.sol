c068a40beb79317b58ec8d0590bb56dc4ad0cee5
JChoy <jchoyeclipse@gmail.com>
2023-07-31 22:51:25 +0900

    feat: implement DepositController

--- contracts/DepositController.sol
+++ contracts/DepositController.sol
@@ -3,6 +3,8 @@
 
 pragma solidity ^0.8.0;
 
+import {IWETH9} from "./external/weth/IWETH9.sol";
+import {IERC721Permit} from "./interfaces/IERC721Permit.sol";
 import {IDepositController} from "./interfaces/IDepositController.sol";
 import {IBondPositionManager} from "./interfaces/IBondPositionManager.sol";
 import {BondPosition} from "./libraries/BondPosition.sol";
@@ -34,16 +36,35 @@ contract DepositController is IDepositController, Controller {
         uint16 lockEpochs,
         uint256 minInterestEarned,
         PermitParams calldata tokenPermitParams
-    ) external payable returns (uint256 positionId) {}
+    ) external payable returns (uint256 positionId) {
+        _permitERC20(currency, amount, tokenPermitParams);
+        bool isNative = currency.isNative();
+        bytes memory data = abi.encode(msg.sender, isNative, minInterestEarned, 0);
+        positionId = _bondManager.mint(isNative ? address(_weth) : Currency.unwrap(currency), amount, lockEpochs, address(this), data);
+        _bondManager.transferFrom(address(this), msg.sender, positionId);
+        _flush(currency, msg.sender);
+    }
 
     function withdraw(
         uint256 positionId,
         uint256 withdrawAmount,
         uint256 maxInterestPaid,
         PermitParams calldata positionPermitParams
-    ) external {}
+    ) external {
+        _permitERC721(IERC721Permit(_bondManager), positionId, positionPermitParams);
+        BondPosition memory position = _bondManager.getPosition(positionId);
+        Currency currency = Currency.wrap(position.asset);
+        bytes memory data = abi.encode(msg.sender, currency.isNative(), 0, maxInterestPaid);
+        _bondManager.adjustPosition(positionId, position.amount - withdrawAmount, position.expiredWith, data);
+        _flush(currency, msg.sender);
+    }
 
-    function collect(uint256 positionId, PermitParams calldata positionPermitParams) external {}
+    function collect(uint256 positionId, PermitParams calldata positionPermitParams) external {
+        _permitERC721(IERC721Permit(_bondManager), positionId, positionPermitParams);
+        Currency currency = Currency.wrap(_bondManager.getPosition(positionId).asset);
+        _bondManager.burnExpiredPosition(positionId);
+        _flush(currency, msg.sender);
+    }
 
     function bondPositionAdjustCallback(
         uint256,
@@ -52,13 +73,37 @@ contract DepositController is IDepositController, Controller {
         Coupon[] memory couponsMinted,
         Coupon[] memory couponsToBurn,
         bytes calldata data
-    ) external {}
+    ) external {
+        require(msg.sender == address(_bondManager));
+        (address user, bool useNative, uint256 sellThreshold, uint256 buyThreshold) =
+            abi.decode(data, (address, bool, uint256, uint256));
+        Currency currency = Currency.wrap(newPosition.asset);
+        uint256 earnedAmount;
+
+        if (couponsMinted.length > 0) {
+            earnedAmount = _sellCoupons(couponsMinted, sellThreshold, currency, user);
+        }
+
+        if (oldPosition.amount < newPosition.amount) {
+            uint256 amountNeeded = newPosition.amount - oldPosition.amount - earnedAmount;
+            if (useNative) {
+                IWETH9(currency.unwrap()).deposit{value: amountNeeded}();
+            } else {
+                currency.transferFrom(user, address(this), amountNeeded);
+            }
+        }
+
+        if (couponsToBurn.length > 0) {
+            _buyCoupons(couponsToBurn, buyThreshold, user, currency, useNative);
+        }
+    }
 
     function getCouponMarket(CouponKey memory couponKey) external view returns (address) {
         return _couponMarkets[couponKey.toId()];
     }
 
     function setCouponMarket(CouponKey memory couponKey, address cloberMarket) external onlyOwner {
+        Currency.wrap(couponKey.asset).approve(address(_bondManager), type(uint256).max);
         return _setCouponMarket(couponKey, cloberMarket);
     }
 }
--- contracts/libraries/Controller.sol
+++ contracts/libraries/Controller.sol
@@ -3,9 +3,12 @@
 
 pragma solidity ^0.8.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+import {CloberMarketSwapCallbackReceiver} from "../external/clober/CloberMarketSwapCallbackReceiver.sol";
 import {CloberMarketFactory} from "../external/clober/CloberMarketFactory.sol";
 import {IWETH9} from "../external/weth/IWETH9.sol";
 import {IWrapped1155Factory} from "../external/wrapped1155/IWrapped1155Factory.sol";
@@ -16,9 +19,11 @@ import {Coupon, CouponLibrary} from "./Coupon.sol";
 import {CouponKey, CouponKeyLibrary} from "./CouponKey.sol";
 import {Currency, CurrencyLibrary} from "./Currency.sol";
 import {Wrapped1155MetadataBuilder} from "./Wrapped1155MetadataBuilder.sol";
+import {IERC721Permit} from "../interfaces/IERC721Permit.sol";
 
-abstract contract Controller is ERC1155Holder, Ownable {
+abstract contract Controller is ERC1155Holder, CloberMarketSwapCallbackReceiver, Ownable {
     error InvalidMarket();
+    error ControllerSlippage();
 
     using CurrencyLibrary for Currency;
     using PermitParamsLibrary for PermitParams;
@@ -37,6 +42,122 @@ abstract contract Controller is ERC1155Holder, Ownable {
         _cloberMarketFactory = CloberMarketFactory(cloberMarketFactory);
         _couponManager = ICouponManager(couponManager);
         _weth = IWETH9(weth);
+    }
+
+    function _sellCoupons(Coupon[] memory coupons, uint256 minEarnedAmount, Currency currency, address user)
+        internal
+        returns (uint256 earnedAmount)
+    {
+        // wrap 1155 to 20
+        _couponManager.safeBatchTransferFrom(
+            address(this),
+            address(_wrapped1155Factory),
+            coupons,
+            Wrapped1155MetadataBuilder.buildWrapped1155BatchMetadata(coupons)
+        );
+        // sell
+        earnedAmount = currency.balanceOfSelf();
+        for (uint256 i = 0; i < coupons.length; ++i) {
+            CloberOrderBook market = CloberOrderBook(_couponMarkets[coupons[i].id()]);
+            market.marketOrder(
+                address(this), 0, 0, coupons[i].amount, 2, abi.encode(user, false)
+            );
+        }
+        earnedAmount = currency.balanceOfSelf() - earnedAmount;
+        if (earnedAmount < minEarnedAmount) {
+            revert ControllerSlippage();
+        }
+    }
+
+    function _buyCoupons(Coupon[] memory coupons, uint256 maxAmountToPay, address user, Currency currency, bool useNative) internal {
+        // buy
+        uint256[] memory tokenIds = new uint256[](coupons.length);
+        uint256[] memory amounts = new uint256[](coupons.length);
+        uint256 paidAmount = useNative ? address(this).balance : IERC20(Currency.unwrap(currency)).balanceOf(user);
+        for (uint256 i = 0; i < coupons.length; ++i) {
+            CloberOrderBook market;
+            {
+                uint256 couponId = coupons[i].id();
+                market = CloberOrderBook(_couponMarkets[couponId]);
+                tokenIds[i] = couponId;
+            }
+            amounts[i] = coupons[i].amount;
+            uint256 dy = coupons[i].amount - IERC20(market.baseToken()).balanceOf(address(this));
+            market.marketOrder(
+                address(this),
+                type(uint16).max,
+                type(uint64).max,
+                dy,
+                1,
+                abi.encode(user, useNative)
+            );
+        }
+        paidAmount = paidAmount - (useNative ? address(this).balance : IERC20(Currency.unwrap(currency)).balanceOf(user));
+        if (paidAmount > maxAmountToPay) {
+            revert ControllerSlippage();
+        }
+
+        // unwrap
+        _wrapped1155Factory.batchUnwrap(
+            address(_couponManager),
+            tokenIds,
+            amounts,
+            address(this),
+            Wrapped1155MetadataBuilder.buildWrapped1155BatchMetadata(coupons)
+        );
+    }
+
+    function cloberMarketSwapCallback(address inputToken, address, uint256 inputAmount, uint256, bytes calldata data)
+        external
+        payable
+    {
+        // check if caller is registered market
+        if (_cloberMarketFactory.getMarketHost(msg.sender) == address(0)) {
+            revert("");
+        }
+
+        (address payer, bool useNative) = abi.decode(data, (address, bool));
+
+        // transfer input tokens
+        if (inputAmount > 0) {
+            if (useNative) {
+                IWETH9(inputToken).deposit{value: inputAmount}();
+                IWETH9(inputToken).transfer(msg.sender, inputAmount);
+            } else {
+                Currency inputCurrency = Currency.wrap(inputToken);
+                uint256 thisBalance = inputCurrency.balanceOfSelf();
+                if (thisBalance < inputAmount) {
+                    inputCurrency.transferFrom(payer, msg.sender, inputAmount - thisBalance);
+                    inputCurrency.transfer(msg.sender, thisBalance);
+                } else {
+                    inputCurrency.transfer(msg.sender, inputAmount);
+                }
+            }
+        }
+    }
+
+    function _permitERC20(Currency currency, uint256 amount, PermitParams calldata p) internal {
+        if (!p.isEmpty()) {
+            IERC20Permit(currency.unwrap()).permit(msg.sender, address(this), amount, p.deadline, p.v, p.r, p.s);
+        }
+    }
+
+    function _permitERC721(IERC721Permit permitNFT, uint256 positionId, PermitParams calldata p) internal {
+        if (!p.isEmpty()) {
+            permitNFT.permit(address(this), positionId, p.deadline, p.v, p.r, p.s);
+        }
+    }
+
+    function _flush(Currency currency, address to) internal {
+        uint256 leftAmount = currency.balanceOfSelf();
+        if (leftAmount == 0) {
+            return;
+        }
+        if (currency.unwrap() == address(_weth)) {
+            _weth.withdraw(leftAmount);
+            currency = CurrencyLibrary.NATIVE;
+        }
+        currency.transfer(to, leftAmount);
     }
 
     function _setCouponMarket(CouponKey memory couponKey, address cloberMarket) internal {
