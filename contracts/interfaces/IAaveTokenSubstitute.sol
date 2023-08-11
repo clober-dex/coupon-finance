fa3bb1ce5c8d68d031938870bd52b0076defb516
JChoy <jchoyeclipse@gmail.com>
2023-08-11 13:45:26 +0900

    feat: add aToken() to interface

--- contracts/interfaces/IAaveTokenSubstitute.sol
+++ contracts/interfaces/IAaveTokenSubstitute.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import {ISubstitute} from "./ISubstitute.sol";
 
 interface IAaveTokenSubstitute is ISubstitute {
+    function aToken() external view returns (address);
+
     function mintByAToken(uint256 amount, address to) external;
 
     function burnToAToken(uint256 amount, address to) external;
