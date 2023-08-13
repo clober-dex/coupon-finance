0904618754d486319578639163fccce6ec156bd9
JChoy <jchoyeclipse@gmail.com>
2023-08-14 01:09:05 +0900

    feat: add coupon tasks

--- task/loanManager.ts
+++ task/loanManager.ts
@@ -29,3 +29,18 @@ task('loan:set-configuration')
       console.log('Registered pair at tx', receipt.transactionHash)
     }
   })
+
+task('loan:get-configuration')
+  .addParam('collateral', 'the name of the collateral asset')
+  .addParam('debt', 'the name of the debt asset')
+  .setAction(async ({ collateral, debt }, hre) => {
+    const manager = await getDeployedContract<LoanPositionManager>('LoanPositionManager')
+    const chainId = hre.network.config.chainId ?? hardhat.id
+    const collateralToken = AAVE_SUBSTITUTES[chainId][collateral]
+    const debtToken = AAVE_SUBSTITUTES[chainId][debt]
+    const configuration = await manager.getLoanConfiguration(collateralToken, debtToken)
+    console.log('liquidationThreshold', configuration.liquidationThreshold / 10 ** 6)
+    console.log('liquidationFee', configuration.liquidationFee / 10 ** 6)
+    console.log('liquidationProtocolFee', configuration.liquidationProtocolFee / 10 ** 6)
+    console.log('liquidationTargetLtv', configuration.liquidationTargetLtv / 10 ** 6)
+  })
--- utils/contract.ts
+++ utils/contract.ts
@@ -1,4 +1,4 @@
-import { Contract, ContractTransaction } from 'ethers'
+import { BigNumber, Contract, ContractTransaction } from 'ethers'
 
 import { getHRE } from './misc'
 
@@ -11,4 +11,37 @@ export const getDeployedContract = async <T extends Contract>(contractName: stri
   const deployments = await hre.deployments.get(contractName)
   const contract = await hre.ethers.getContractAt(deployments.abi, deployments.address)
   return contract as T
+}
+
+export const buildWrapped1155Metadata = async (tokenAddress: string, epoch: number): Promise<string> => {
+  const hre = getHRE()
+  const token = await hre.ethers.getContractAt('IERC20Metadata', tokenAddress)
+  const tokenSymbol = await token.symbol()
+  const epochString = epoch.toString()
+  const addLength = tokenSymbol.length + epochString.length
+  const nameData = hre.ethers.utils.solidityPack(
+    ['string', 'string', 'string', 'string'],
+    [tokenSymbol, ' Bond Coupon (', epochString, ')'],
+  )
+  const symbolData = hre.ethers.utils.solidityPack(['string', 'string', 'string'], [tokenSymbol, '-CP', epochString])
+  const decimal = await token.decimals()
+  return hre.ethers.utils.solidityPack(
+    ['bytes32', 'bytes32', 'bytes1'],
+    [
+      BigNumber.from(nameData)
+        .add(30 + addLength)
+        .toHexString(),
+      BigNumber.from(symbolData)
+        .add(30 + addLength)
+        .toHexString(),
+      decimal,
+    ],
+  )
+}
+
+export const convertToCouponId = (tokenAddress: string, epoch: number): BigNumber => {
+  const hre = getHRE()
+  return BigNumber.from(epoch)
+    .shl(160)
+    .add(BigNumber.from(hre.ethers.utils.getAddress(tokenAddress)))
 }
