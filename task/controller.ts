05530c11b3a1d7abce1c627f865a6fa92ac3292e
JChoy <jchoyeclipse@gmail.com>
2023-08-14 13:43:09 +0900

    fix: fix controller task bug

--- task/controller.ts
+++ task/controller.ts
@@ -8,14 +8,15 @@ task('borrow-controller:set-allowances').setAction(async (taskArgs, hre) => {
   const controller = await getDeployedContract<BorrowController>('BorrowController')
   const loanManager = await getDeployedContract<LoanPositionManager>('LoanPositionManager')
   const chainId = hre.network.config.chainId ?? hardhat.id
-  const tokenAddresses = Object.keys(AAVE_SUBSTITUTES[chainId])
-  for (const tokenAddress of tokenAddresses) {
-    const token = await hre.ethers.getContractAt('IERC20Metadata', tokenAddress)
+  const tokenNames = Object.keys(AAVE_SUBSTITUTES[chainId])
+  const tokenAddresses = Object.values(AAVE_SUBSTITUTES[chainId])
+  for (let i = 0; i < tokenNames.length; i++) {
+    const token = await hre.ethers.getContractAt('IERC20Metadata', tokenAddresses[i])
     if ((await token.allowance(controller.address, loanManager.address)).gt(0)) {
-      console.log(`Allowance already set for ${tokenAddress}`)
+      console.log(`Allowance already set for ${tokenNames[i]}`)
     } else {
       const receipt = await waitForTx(controller.setCollateralAllowance(token.address))
-      console.log(`Set allowance for ${tokenAddress} at ${receipt.transactionHash}`)
+      console.log(`Set allowance for ${tokenNames[i]} at ${receipt.transactionHash}`)
     }
   }
 })
