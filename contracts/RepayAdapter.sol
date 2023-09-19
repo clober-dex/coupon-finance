522b52078f90815dcbcdff200006e13b2481c63d
Sean <detectivekim1@gmail.com>
2023-09-19 19:04:19 +0900

    refactor: `setCollateralAllowance` => `giveLoanManagerAllowance`

--- contracts/RepayAdapter.sol
+++ contracts/RepayAdapter.sol
@@ -119,7 +119,7 @@ contract RepayAdapter is IRepayAdapter, Controller, IPositionLocker {
         ISubstitute(debt).mint(outAmount, address(this));
     }
 
-    function setCollateralAllowance(address collateralToken) external onlyOwner {
-        IERC20(collateralToken).approve(address(_loanManager), type(uint256).max);
+    function giveLoanManagerAllowance(address token) external onlyOwner {
+        IERC20(token).approve(address(_loanManager), type(uint256).max);
     }
 }
